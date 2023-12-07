203b5a2e67bbee3cc387d22dc52457700304b839
JouanelR <73125997+JouanelR@users.noreply.github.com>
2023-12-08 00:55:49 +0100

    add signup fonction

---
+++ server.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template, redirect, url_for
+import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+db_config = {
+    'host': os.getenv('POSTGRES_HOST'),
+    'database': os.getenv('POSTGRES_DB'),
+    'user': os.getenv('POSTGRES_USER'),
+    'password': os.getenv('POSTGRES_PASSWORD')
+}
+
+def get_db_connection():
+    conn = psycopg2.connect(**db_config)
+    return conn
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        userDetails = request.form
+        username = userDetails['username']
+        email = userDetails['email']
+        password = userDetails['password']  # À hasher avant de stocker
+        conn = get_db_connection()
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)", (username, email, password))
+        conn.commit()
+        cursor.close()
+        conn.close()
+        return redirect(url_for('login'))
+    return render_template('signup.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        pass
+    return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
